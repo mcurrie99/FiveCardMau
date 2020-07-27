71b627464a3639713554fda8c2e4c7a4b577ceb0
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2020-07-26 21:48:29 -0400

    update ip address to connect to

--- Network.py
+++ Network.py
@@ -5,7 +5,7 @@ import struct
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = '98.109.68.217'
+        self.server = '98.109.48.234'
         self.port = 5555
         self.addr = (self.server, self.port)
 
