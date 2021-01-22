a197fc50f800c98e338d4c8e7be1874bed3611bb
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2021-01-21 23:41:22 -0500

    Hides IP Address

--- Network.py
+++ Network.py
@@ -11,7 +11,7 @@ class Network:
         # self.server = '98.109.48.234'
 
         # IP address of computer at home
-        self.server = '96.242.117.209'
+        self.server = # Public IP Address of the server (do not include port)
 
         # IP address at Purdue
         #self.server = '128.211.222.85'
